2cce9c61971642582578b922d9f01132cff88b25
Jeremiah <charlesjeremiah204@gmail.com>
2025-11-22 02:32:25 -0800

    Updating hero and service

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+          <div className="navbar">
+              <div className="logo">
+                <img src="src/assets/image 1.png" alt="" />
+              </div>
+              <div className="nav-link">
+                  <a href="#">About Us</a>
+                  <a href="#">Projects</a>
+                  <a href="#">Services</a>
+                  <a href="#">How we work</a>
+              </div>
+              <div className="btn">Start your Project</div>
+          </div>
+      </nav>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Hero from './hero.jsx'
+import Service from './service.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Hero />
+    <Service />
+  </StrictMode>,
+)
