9291cf88e60f2acfdf71691f191383eccdcf49b6
jeremiahbwala <charlesjeremiah204@gmail.com>
2025-11-27 08:35:27 +0100

    Remove Hero, Service, and CaseStudy components
    Removed unused components from the render method.

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Hero from './hero.jsx'
-import Service from './service.jsx'
-import CaseStudy from './CaseStudy.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CaseStudy />
     <App />
-    <Hero />
-    <Service />
   </StrictMode>,
 )
